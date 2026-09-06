a434b4b5660eda4b59ef49a85c9d3351f18233d0
aletheios42 <alejandropintosdev@gmail.com>
2026-09-07 00:47:00 +0200

    feat: añadido comprobacion entre while y for

---
+++ while_vs_for/for.c
@@ -0,0 +1,7 @@
+int main() {
+  int total = 0;
+
+  for (int i = 0; i < 10; i++) {
+    total += i;
+  }
+}
---
+++ while_vs_for/while.c
@@ -0,0 +1,9 @@
+int main() {
+  int i = 0;
+  int total = 0;
+
+  while (i < 10) {
+    total += i;
+    i++;
+  }
+}
